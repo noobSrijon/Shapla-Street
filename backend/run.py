1f9fd9845cbab32ed514981e4aaeb2b001e3f6e3
Srijon <srijonkumar18@gmail.com>
2026-02-11 00:52:54 -0600

    loaded port from env

--- backend/run.py
+++ backend/run.py
@@ -7,4 +7,5 @@ load_dotenv()
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
